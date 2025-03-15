ab41a12b5c9693ee9e7ae20a0cc395cfef2177b7
rondoudou <marcpon@yahoo.fr>
2025-03-15 21:15:23 +0100

    functional game_manager from lib-rpg + display of characters

--- src/application.rs
+++ src/application.rs
@@ -1,15 +1,20 @@
+use dioxus::prelude::server_fn;
+use dioxus::{prelude::server, prelude::ServerFnError};
 use lib_rpg::game_manager::GameManager;
+use serde::Deserialize;
+use serde::Serialize;
 
-#[derive(Default, Debug, Clone, PartialEq)]
+#[derive(Default, Debug, Clone, PartialEq, Deserialize, Serialize)]
 pub struct Application {
     pub game_manager: GameManager,
 }
 
-impl Application {
-    pub fn try_new() -> Result<Application, String> {
-        match GameManager::try_new() {
-            Ok(gm) => Ok(Application { game_manager: gm }),
-            Err(e) => Err(format!("Failed to create GameManager: {}", e)),
-        }
+#[server]
+pub async fn try_new() -> Result<Application, ServerFnError> {
+    match GameManager::try_new() {
+        Ok(gm) => Ok(Application { game_manager: gm }),
+        Err(_) => Err(ServerFnError::Request(
+            "Failed to create GameManager".to_string(),
+        )),
     }
 }
