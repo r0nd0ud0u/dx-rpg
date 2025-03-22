46001567ef32c76d74224f0121d8230ca7bbb438
r0nd0ud0u <57643172+r0nd0ud0u@users.noreply.github.com>
2025-03-22 10:18:36 +0100

    add energy bars to character panel + add grid gameBoard  with characters panels (#5)
    * add energy bars
    
    * character panel with photo
    
    * update hero panel with photo
    
    * add gameboard grid + change color on hero or boss container
    
    * update readme
    
    * update readme
    
    * review

--- src/character_page.rs
+++ src/character_page.rs
@@ -1,42 +1,77 @@
 use dioxus::prelude::*;
-use lib_rpg::character::Character;
+use lib_rpg::character::{Character, CharacterType};
+
+pub const PATH_IMG: &str = "assets/img";
 
 #[component]
-pub fn Character_page(c: Character) -> Element {
-    let max_life = c.stats.hp.max;
-    let mut life = use_signal(|| c.stats.hp.current);
+pub fn CharacterPanel(c: Character) -> Element {
+    let bg = if c.kind == CharacterType::Hero { "blue" } else { "red" };
     rsx! {
-        div { id: "character",
-            h4 { {c.name} }
-            div { class: "container-bar",
-                div {
-                    class: "life-bar",
-                    width: "{life}%",
-                    background_color: get_color(life() as i32),
+        div { class: "character",
+            background_color: bg,
+            div {  
+                img {
+                    src: format!("{}/{}.png", PATH_IMG, c.photo_name.clone()),
+                    class: "image-small",
                 }
-                span { class: "bar-text", "{life()} / {max_life}" }
+                h4 { {c.name.clone()} }
             }
-        }
-
-        button {
-            class: "damages-btn",
-            onclick: move |_| {
-                let new_life = life().clone() as i32 - 10;
-                if new_life < 0 {
-                    life.set(0);
-                } else {
-                    life.set(new_life as u32);
+            div {
+                if c.stats.hp.max > 0 {
+                    BarComponent {
+                        max: c.stats.hp.max,
+                        current: c.stats.hp.current,
+                        name: "HP",
+                    }
                 }
-            },
-            "Give damages"
+                if c.stats.mana.max > 0 {
+                    BarComponent {
+                        max: c.stats.mana.max,
+                        current: c.stats.mana.current,
+                        name: "MP",
+                    }
+                }
+                if c.stats.vigor.max > 0 {
+                    BarComponent {
+                        max: c.stats.vigor.max,
+                        current: c.stats.vigor.current,
+                        name: "VP",
+                    }
+                }
+                if c.stats.berseck.max > 0 {
+                    BarComponent {
+                        max: c.stats.berseck.max,
+                        current: c.stats.berseck.current,
+                        name: "BP",
+                    }
+                }
+            }
         }
     }
 }
 
-fn get_color(life: i32) -> &'static str {
-    if life > 80 {
+#[component]
+pub fn BarComponent(max: u32, current: u32, name: String) -> Element {
+    let mut current_sig = use_signal(|| current);
+    rsx! {
+        div { class: "grid-container",
+            h4 { {name} }
+            div { class: "container-bar",
+                div {
+                    class: "life-bar",
+                    width: "{current_sig()}%",
+                    background_color: get_color(current_sig() as i32),
+                }
+            }
+            h4 { "{current_sig()} / {max}" }
+        }
+    }
+}
+
+fn get_color(value: i32) -> &'static str {
+    if value > 80 {
         "green"
-    } else if life > 20 {
+    } else if value > 20 {
         "orange"
     } else {
         "red"
