2b3df7cea25821470f8ce137866fb35624cf618b
r0nd0ud0u <57643172+r0nd0ud0u@users.noreply.github.com>
2025-05-08 20:08:41 +0200

    Origin/8 use gradient for energy bar (#23)
    * wip gradient defined with application, not working
    
    * init gradient directly in component
    
    * fmt
    
    * use once_cell to initiate a gradient

--- src/common.rs
+++ src/common.rs
@@ -1,5 +1,14 @@
 use dioxus::prelude::*;
 
 use crate::application::Application;
+use colorgrad::{CatmullRomGradient, GradientBuilder};
+use once_cell::sync::Lazy;
 
 pub static APP: GlobalSignal<Application> = Signal::global(Application::default);
+
+pub static ENERGY_GRAD: Lazy<CatmullRomGradient> = Lazy::new(|| {
+    GradientBuilder::new()
+        .html_colors(&["deeppink", "gold", "seagreen"])
+        .build::<CatmullRomGradient>()
+        .expect("Failed to build gradient")
+});
