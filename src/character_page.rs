90361321d85cd8bebd748246592f5b3fd9c12b29
r0nd0ud0u <57643172+r0nd0ud0u@users.noreply.github.com>
2025-06-05 02:12:32 +0200

    Sync multiplayer conn (#38)
    * sync 2 connections on SERVER to play game
    
    (cherry picked from commit d40a7ea3b8d6f802b937b84f81476e966743840d)
    
    * extract the signals used in arg for asnyc functions
    
    * test character page
    
    * tempo auto boss atk is working on 2 pc sync
    
    * update
    
    * atk on going tag sync
    
    * update for ATK tag hero/boss
    
    * fix bug auto atk
    
    * fix display result before auto atk
    
    * remove tempo display result atk
    
    * wip
    
    * update auto atk
    
    * fmt
    
    * update submodule

--- src/character_page.rs
+++ src/character_page.rs
@@ -5,13 +5,9 @@ use lib_rpg::{
     attack_type::AttackType,
     character::{Character, CharacterType},
     common::stats_const::*,
-    game_manager::ResultLaunchAttack,
 };
 
-use crate::{
-    application,
-    common::{tempo_const::AUTO_ATK, APP, ENERGY_GRAD},
-};
+use crate::common::{APP, ENERGY_GRAD};
 
 pub const PATH_IMG: &str = "assets/img";
 
@@ -19,11 +15,10 @@ pub const PATH_IMG: &str = "assets/img";
 pub fn CharacterPanel(
     c: Character,
     current_player_name: String,
-    is_auto_atk: bool,
     selected_atk: Signal<AttackType>,
     atk_menu_display: Signal<bool>,
-    result_auto_atk: Signal<ResultLaunchAttack>,
-    output_auto_atk: Signal<ResultLaunchAttack>,
+    write_game_manager: Signal<bool>,
+    is_auto_atk: ReadOnlySignal<bool>,
 ) -> Element {
     // if boss is dead, panel is hidden
     if c.is_dead().is_some_and(|value| value) && c.kind == CharacterType::Boss {
@@ -40,18 +35,9 @@ pub fn CharacterPanel(
         (VIGOR.to_owned(), "VP".to_owned()),
         (BERSECK.to_owned(), "BP".to_owned()),
     ]);
-    let _ = use_resource(use_reactive!(|(is_auto_atk,)| async move {
-        // Simulate a delay before launching the attack
-        // We manually add the resource to the dependencies list with the `use_reactive` hook
-        // Any time `is_auto_atk` changes, the resource will rerun
-        if is_auto_atk {
-            // TODO random attak from boss or pattern
-            let _ = application::sleep_from_millis(AUTO_ATK).await;
-            output_auto_atk.set(APP.write().game_manager.launch_attack("SimpleAtk"));
-            selected_atk.set(AttackType::default());
-        }
-    }));
 
+    let name2 = c.name.clone();
+    let kind2 = c.kind.clone();
     rsx! {
         div { class: "character", background_color: bg,
             div {
@@ -74,15 +60,13 @@ pub fn CharacterPanel(
             }
         }
         // atk button
-        if is_auto_atk {
+        if is_auto_atk() {
             button { class: "atk-button-ennemy", onclick: move |_| async move {}, "ATK On Going" }
-        } else if c.kind == CharacterType::Hero && current_player_name == c.name {
+        } else if kind2.clone() == CharacterType::Hero && current_player_name == name2.clone() {
             button {
                 class: "menu-atk-button",
                 onclick: move |_| async move {
                     atk_menu_display.set(!atk_menu_display());
-                    result_auto_atk.set(ResultLaunchAttack::default());
-                    output_auto_atk.set(ResultLaunchAttack::default());
                 },
                 "ATK"
             }
@@ -92,17 +76,21 @@ pub fn CharacterPanel(
             class: "character-name-button",
             background_color: "black",
             onclick: move |_| async move {},
-            "{c.name}"
+            "{name2.clone()}"
         }
         // target button
         if !selected_atk().name.is_empty() {
-            CharacterTargetButton { c: c.clone(), selected_atk }
+            CharacterTargetButton { c: c.clone(), selected_atk, write_game_manager }
         }
     }
 }
 
 #[component]
-pub fn CharacterTargetButton(c: Character, selected_atk: Signal<AttackType>) -> Element {
+pub fn CharacterTargetButton(
+    c: Character,
+    selected_atk: Signal<AttackType>,
+    write_game_manager: Signal<bool>,
+) -> Element {
     let mut kind_str = "hero";
     if c.kind == CharacterType::Boss {
         kind_str = "boss";
@@ -124,6 +112,7 @@ pub fn CharacterTargetButton(c: Character, selected_atk: Signal<AttackType>) -> 
                             .game_manager
                             .pm
                             .set_one_target(&new_target_name, &selected_atk().reach);
+                        write_game_manager.set(true);
                     }
                 },
                 ""
@@ -156,6 +145,7 @@ pub fn NewAtkButton(
     display_atklist_sig: Signal<bool>,
     selected_atk: Signal<AttackType>,
     launcher: Character,
+    write_game_manager: Signal<bool>,
 ) -> Element {
     rsx! {
         button {
@@ -168,6 +158,7 @@ pub fn NewAtkButton(
                     APP.write().game_manager.pm.set_targeted_characters(&l_launcher, &value);
                     *display_atklist_sig.write() = false;
                     selected_atk.set(value);
+                    write_game_manager.set(true);
                 }
             },
             "{attack_type.name}"
@@ -180,6 +171,7 @@ pub fn AttackList(
     name: String,
     display_atklist_sig: Signal<bool>,
     selected_atk: Signal<AttackType>,
+    write_game_manager: Signal<bool>,
 ) -> Element {
     if let Some(c) = APP.read().game_manager.pm.get_active_character(&name) {
         rsx! {
@@ -198,6 +190,7 @@ pub fn AttackList(
                                 display_atklist_sig,
                                 selected_atk,
                                 launcher: c.clone(),
+                                write_game_manager,
                             }
                             button {
                                 class: "cost-energy-button",
