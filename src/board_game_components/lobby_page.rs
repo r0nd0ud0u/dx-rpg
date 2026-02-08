49c937f699c0d423722894cea7a153b38f9efc42
r0nd0ud0u <57643172+r0nd0ud0u@users.noreply.github.com>
2026-02-08 14:57:01 +0100

    151 remove page mode for start game (#152)
    * use lobby page as game page
    
    * review
    
    * fmt

--- src/board_game_components/lobby_page.rs
+++ src/board_game_components/lobby_page.rs
@@ -4,12 +4,12 @@ use dioxus::{
 };
 
 use crate::{
-    application::Application,
     board_game_components::{
-        character_select::CharacterSelect, common_comp::ButtonLink,
-        msg_from_client::send_start_game,
+        character_select::CharacterSelect, msg_from_client::send_start_game,
+        startgame_page::StartGamePage,
     },
-    common::Route,
+    common::SERVER_NAME,
+    components::button::Button,
     websocket_handler::{
         event::{ClientEvent, ServerEvent},
         game_state::ServerData,
@@ -21,22 +21,27 @@ pub fn LobbyPage() -> Element {
     // contexts
     let socket = use_context::<UseWebsocket<ClientEvent, ServerEvent, CborEncoding>>();
     let server_data = use_context::<Signal<ServerData>>();
+    let local_login_name_session = use_context::<Signal<String>>();
 
     rsx! {
-        div { class: "home-container",
-            h1 { "LobbyPage" }
-
-            if server_data.read().app.is_game_running {
-                ButtonLink {
-                    target: Route::StartGamePage {}.into(),
-                    name: "Start Game".to_string(),
-                    onclick: move |_| async move {
-                        send_start_game(socket).await;
-                    },
+        // if the game is not running, show the lobby page, otherwise show the start game page
+        if !server_data.read().app.is_game_running {
+            div { class: "home-container",
+                h1 { "LobbyPage" }
+                // if the current client is the host, show start game button
+                if SERVER_NAME() == local_login_name_session() {
+                    Button {
+                        onclick: move |_| async move {
+                            send_start_game(socket).await;
+                        },
+                        "Start Game"
+                    }
                 }
+                // show character select page
                 CharacterSelect {}
             }
-
+        } else {
+            StartGamePage {}
         }
 
     }
