003f6c876003bb05dfeb5ffa97d3e8aa3a4b59cc
r0nd0ud0u <57643172+r0nd0ud0u@users.noreply.github.com>
2026-03-01 19:30:50 +0100

    Add data manager (#189)
    * wip
    
    * finished

--- src/websocket_handler/event.rs
+++ src/websocket_handler/event.rs
@@ -1,7 +1,7 @@
 #[cfg(feature = "server")]
 use crate::application::init_application;
 #[cfg(feature = "server")]
-use crate::common::{SAVED_APP, SAVED_APP_REPLAY};
+use crate::common::{DATA_MANAGER, SAVED_APP, SAVED_APP_REPLAY};
 use crate::websocket_handler::game_state::GamePhase;
 use crate::websocket_handler::game_state::OnGoingGame;
 use crate::websocket_handler::game_state::ServerData;
@@ -76,8 +76,8 @@ pub enum ClientEvent {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum ServerEvent {
+    InitClient(u32, Vec<String>),           // player id, characters list
     NewClientOnExistingPlayer(String, u32), // welcome message, player id
-    AssignPlayerId(u32),                    // player id
     ReconnectAllSessions(String, i64),      // username, sql-id
     UpdateServerData(Box<ServerData>),      // server data
     UpdateOngoingGames(Vec<OnGoingGame>),
@@ -112,8 +112,18 @@ pub async fn on_rcv_client_event(
                 clients.insert(client_id as usize, tx);
                 tracing::info!("Client {} connected (total: {})", client_id, clients.len());
             }
+            // Data manager
+    let dm = {
+        let dm = DATA_MANAGER.lock().unwrap();
+        dm.clone()
+    };
+    let character_list = dm
+        .all_heroes
+        .iter()
+        .map(|h| h.name.clone())
+        .collect::<Vec<String>>();
 
-            let _ = socket.send(ServerEvent::AssignPlayerId(client_id)).await;
+            let _ = socket.send(ServerEvent::InitClient(client_id, character_list)).await;
             let _ = socket.send(ServerEvent::NewClientOnExistingPlayer(format!("Welcome! (id={})", client_id), client_id)).await;
 
             // Main loop: handle incoming socket messages and outgoing queued messages
@@ -234,7 +244,7 @@ pub async fn on_rcv_client_event(
 
 #[cfg(feature = "server")]
 pub fn add_player(name: String, id: u32) {
-    let mut sm = SERVER_MANAGER.lock().unwrap();
+    let mut sm: std::sync::MutexGuard<'_, GameStateManager> = SERVER_MANAGER.lock().unwrap();
     sm.players.entry(name.clone()).or_default().push(id);
     tracing::info!("All connected players: {:?}", sm.players);
 }
@@ -474,41 +484,45 @@ pub async fn start_new_game_by_player(server_name: &str, is_replay: bool) {
 #[cfg(feature = "server")]
 pub async fn init_new_game_by_player(server_name: &str, id: u32, player_name: &str) {
     // add new ongoing game
-    match Application::try_new().await {
-        Ok(mut app) => {
-            tracing::info!("New application created for player: {}", server_name);
-            // init a new game
-            init_application(server_name, &mut app);
-            // update ongoing servers data list
-            let mut sm = SERVER_MANAGER.lock().unwrap();
-            // remove ongoing game if already exists for the server name
-            sm.ongoing_games
-                .retain(|ongoing_game| ongoing_game.server_name != server_name);
-            // add ongoing game
-            sm.ongoing_games.push(OnGoingGame {
-                path: app.game_manager.game_paths.current_game_dir.clone(),
-                server_name: server_name.to_string(),
-            });
-            drop(sm);
-            // add server data
-            app.game_phase = GamePhase::InitGame;
-            // add first player
-            app.players_nb = 0;
-            add_server_data_with_player(&app, server_name, id, player_name);
-            // update for the clients connected to that server
-            update_clients_server_data(server_name);
-            update_clients_ongoing_games();
-        }
-        Err(_) => tracing::error!("no app"),
-    }
+    let mut app = Application::new().await;
+    tracing::info!("New application created for player: {}", server_name);
+    // init a new game
+    init_application(server_name, &mut app);
+    // update ongoing servers data list
+    let mut sm = SERVER_MANAGER.lock().unwrap();
+    // remove ongoing game if already exists for the server name
+    sm.ongoing_games
+        .retain(|ongoing_game| ongoing_game.server_name != server_name);
+    // add ongoing game
+    sm.ongoing_games.push(OnGoingGame {
+        path: app.game_manager.game_paths.current_game_dir.clone(),
+        server_name: server_name.to_string(),
+    });
+    drop(sm);
+    // add server data
+    app.game_phase = GamePhase::InitGame;
+    // add first player
+    app.players_nb = 0;
+    add_server_data_with_player(&app, server_name, id, player_name);
+    // update for the clients connected to that server
+    update_clients_server_data(server_name);
+    update_clients_ongoing_games();
+}
+
+#[cfg(feature = "server")]
+fn get_current_game_path(player_name: &str, current_game_dir: &str) -> PathBuf {
+    use crate::common::SAVED_DATA;
+    let saved_dir: PathBuf = SAVED_DATA.join(PathBuf::from(player_name));
+
+    saved_dir.join(current_game_dir)
 }
 
 #[cfg(feature = "server")]
 async fn save_application(app: &Application, save_game_name: &str, player_name: &str) {
     // create dir
     use crate::common::SAVED_DATA;
-    let mut saved_dir: PathBuf = SAVED_DATA.join(PathBuf::from(player_name));
-    saved_dir = saved_dir.join(app.game_manager.game_paths.current_game_dir.clone());
+    let saved_dir: PathBuf = SAVED_DATA.join(PathBuf::from(player_name));
+    let saved_dir = saved_dir.join(app.game_manager.game_paths.current_game_dir.clone());
     match application::create_dir(saved_dir.clone()).await {
         Ok(()) => {}
         Err(e) => tracing::error!("Failed to create directory: {}", e),
@@ -707,6 +721,7 @@ fn update_lobby_page_after_joining_game(server_name: &str, player_name: &str, cl
 // Used when GamePhase::InitGame
 #[cfg(feature = "server")]
 fn add_character_on_server_data(server_name: &str, player_name: &str, character_name: &str) {
+    let dm = DATA_MANAGER.lock().unwrap();
     let mut sm = SERVER_MANAGER.lock().unwrap();
     if let Some(server_data) = sm.servers_data.get_mut(server_name) {
         // remove character from all players in server data
@@ -731,13 +746,8 @@ fn add_character_on_server_data(server_name: &str, player_name: &str, character_
                 .character_names
                 .iter()
                 .for_each(|character_name| {
-                    if let Some(character) = server_data
-                        .app
-                        .game_manager
-                        .pm
-                        .all_heroes
-                        .iter()
-                        .find(|h| h.name == *character_name)
+                    if let Some(character) =
+                        dm.all_heroes.iter().find(|h| h.name == *character_name)
                     {
                         server_data
                             .app
@@ -813,6 +823,9 @@ async fn load_game_by_player(
     server_name_opt: Option<String>,
 ) {
     let server_name = server_name_opt.unwrap_or_else(|| player_name.clone());
+
+    // TODO game path should be init at initialization of the game and not here, and it should not be based on player name but on server name, to avoid issues when several players with different names are playing on the same server
+    get_current_game_path(&player_name, game_path.to_str().unwrap_or_default());
 
     let mut app = match application::get_application_by_game_dir(game_path.clone(), is_replay).await
     {
