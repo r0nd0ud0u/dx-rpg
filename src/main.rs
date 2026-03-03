a6f69c18a726c6f9b01b6036a3459f74ed6aa129
r0nd0ud0u <marcpon@yahoo.fr>
2026-03-04 00:04:43 +0100

    fmt

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,11 @@ fn main() {
 
     // Init logger
     dioxus::logger::init(
-    std::env::var("LOG_LEVEL")
-        .unwrap_or_else(|_| "info".to_string())
-        .parse::<Level>()
-        .unwrap_or(Level::INFO),
-);
+        std::env::var("LOG_LEVEL")
+            .unwrap_or_else(|_| "info".to_string())
+            .parse::<Level>()
+            .unwrap_or(Level::INFO),
+    );
     tracing::info!("Rendering app!");
 
     // On the client, we simply launch the app as normal, taking over the main thread
