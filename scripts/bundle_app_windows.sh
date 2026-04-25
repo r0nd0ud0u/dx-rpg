b60c1ec49581a3501bd26d6cbab1cbb13b4c7def
r0nd0ud0u <marcpon@yahoo.fr>
2026-04-25 18:04:27 +0200

    upd script

--- scripts/bundle_app_windows.sh
+++ scripts/bundle_app_windows.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dx bundle --platform web --release --out-dir dx-rpg_windows --package-types "exe"
+dx bundle --platform web --release --out-dir dx-rpg_windows --package-types "msi"
 cp -r offlines dx-rpg_windows/offlines/
 mkdir -p dx-rpg_windows/output/games
 cp .env dx-rpg_windows/.env
